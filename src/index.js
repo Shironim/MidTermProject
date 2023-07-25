ff7bde56bda475d488b0813ebd27ecd5a6db8737
Shironim <dimasseto.id@gmail.com>
2023-07-26 00:51:37 +0700

    Mid Term

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import videoRouter from './routers/video.js';
+import connectDB from './config/db.js';
+import dotenv from 'dotenv';
+import bodyParser from 'body-parser';
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+dotenv.config();
+connectDB();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.json());
+
+app.use('/tokplay', videoRouter);
+
+app.use((err, res) => {
+  res.json({
+    message: err.message
+  })
+})
+
+app.listen(PORT, () => {
+  console.log(`Server berhasil di running http://localhost:${PORT}`);
+})
